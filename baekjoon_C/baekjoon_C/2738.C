4020025b316c7c74fbd70699d5da8c859dcefa02
REHOOOO <104660000+REHOOOO@users.noreply.github.com>
2023-07-03 21:56:35 +0900

    2738

---
+++ baekjoon_C/baekjoon_C/2738.C
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int a[100][100];
+	int b[100][100];
+	int n, m;
+	int i, j;
+
+	scanf("%d %d", &n, &m);
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < m; j++)
+		{
+			scanf("%d", &a[i][j]);
+		}
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < m; j++)
+		{
+			scanf("%d", &b[i][j]);
+		}
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < m; j++)
+		{
+			printf("%d ", a[i][j] + b[i][j]);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
