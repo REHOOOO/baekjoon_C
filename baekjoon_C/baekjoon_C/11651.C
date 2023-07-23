f4dbe242f57b272ae9d7ba9d7f610c30977827bb
REHOOOO <104660000+REHOOOO@users.noreply.github.com>
2023-07-23 18:01:04 +0900

    11651
    2차원 배열에서 qsort를 사용해야함

--- baekjoon_C/baekjoon_C/11651.C
+++ baekjoon_C/baekjoon_C/11651.C
@@ -1,53 +1,46 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>	// qsort 함수
+
+int xy[2][100000];
+
+int compare(const void* a, const void* b)
+{
+	int num1 = *(int*)a; // void 포인터를 int 포인터로 변환한 뒤 역참조하여 값을 가져옴
+	int num2 = *(int*)b; // void 포인터를 int 포인터로 변환한 뒤 역참조하여 값을 가져옴
+
+	if (num1 < num2)
+	{
+		return -1;
+	}
+
+	if (num1 > num2)
+	{
+		return 1;
+	}
+
+	return 0;
+}
 
 int main()
 {
 	int n;
-	int i, j;
-	int temp;
-	int xy[100000][2];
+	int i;
 
 	scanf("%d", &n);
 
 	for (i = 0; i < n; i++)
 	{
-		scanf("%d %d", &xy[i][0], &xy[i][1]);
+		scanf("%d %d", &xy[0][i], &xy[1][i]);
 	}
 
-	for (i = 0; i < n; i++)	// 버블정렬
-	{
-		for (j = 0; j < n - 1 - i; j++)
-		{
-			if (xy[j][1] > xy[j + 1][1])
-			{
-				temp = xy[j][1];
-				xy[j][1] = xy[j + 1][1];
-				xy[j + 1][1] = temp;
+	//qsort(xy[0], n, sizeof(int), compare);
+	qsort(xy, n, sizeof(int), compare);
 
-				temp = xy[j][0];
-				xy[j][0] = xy[j + 1][0];
-				xy[j + 1][0] = temp;
-			}
-			else if (xy[j][1] == xy[j + 1][1])
-			{
-				if (xy[j][0] > xy[j + 1][0])
-				{
-					temp = xy[j][1];
-					xy[j][1] = xy[j + 1][1];
-					xy[j + 1][1] = temp;
-
-					temp = xy[j][0];
-					xy[j][0] = xy[j + 1][0];
-					xy[j + 1][0] = temp;
-				}
-			}
-		}
-	}
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d %d\n", xy[i][0], xy[i][1]);
+		printf("%d %d\n", xy[0][i], xy[1][i]);
 	}
 
 	return 0;
