3900e8c1bdab6ed8caa05e181c338c410e086452
REHOOOO <104660000+REHOOOO@users.noreply.github.com>
2023-07-31 23:19:07 +0900

    3036

---
+++ baekjoon_C/baekjoon_C/3036.C
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int n;
+	int i;
+	int r[100];
+
+	scanf("%d", &n);
+
+	for (i = 0; i < n; i++)
+	{
+		scanf("%d", r[i]);
+	}
+
+
+
+	return 0;
+}
