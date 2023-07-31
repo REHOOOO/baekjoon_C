0f27a556736466cd9d98a21ab6e736f33c229f2c
REHOOOO <104660000+REHOOOO@users.noreply.github.com>
2023-07-31 23:44:00 +0900

    3036

--- baekjoon_C/baekjoon_C/3036.C
+++ baekjoon_C/baekjoon_C/3036.C
@@ -4,17 +4,27 @@
 int main()
 {
 	int n;
-	int i;
+	int i, j;
 	int r[100];
 
 	scanf("%d", &n);
 
 	for (i = 0; i < n; i++)
 	{
-		scanf("%d", r[i]);
+		scanf("%d", &r[i]);
 	}
 
-
+	for (i = 1; i < n; i++)
+	{
+		for (j = r[0]; j > 0; j--)
+		{
+			if (r[0] % j == 0 && r[i] % j == 0)
+			{
+				printf("%d/%d\n", r[0] / j, r[i] / j);
+				break;
+			}
+		}
+	}
 
 	return 0;
 }
