57a5c13b0875426782729ec21f41ac9c017d6213
REHOOOO <104660000+REHOOOO@users.noreply.github.com>
2023-08-16 17:24:28 +0900

    15650

--- baekjoon_C/baekjoon_C/15650.C
+++ baekjoon_C/baekjoon_C/15650.C
@@ -5,7 +5,7 @@ int n, m;
 int arr[8] = { 0, };
 int visit[8] = { 0, };
 
-void dfs(int depth)
+void dfs(int depth, int num)
 {
 	int i;
 	if (depth == m)	// 깊이가 m이면 현재까지 저장했던 수열을 출력
@@ -18,7 +18,7 @@ void dfs(int depth)
 	}
 	else
 	{
-		for (i = 1; i <= n; i++)
+		for (i = num; i <= n; i++)
 		{
 			if (visit[i - 1] == 1)	// 이미 사용한 숫자이면 넘어간다
 			{
@@ -28,7 +28,7 @@ void dfs(int depth)
 			{
 				arr[depth] = i;	// 현재 깊이의 숫자는 i로 저장
 				visit[i - 1] = 1;	// 사용했다는 정보를 저장
-				dfs(depth + 1);	// 다음 깊이로 넘어간다
+				dfs(depth + 1, i);	// 다음 깊이로 넘어간다 / 다음 깊이에서 시작 숫자는 i가 된다
 				visit[i - 1] = 0;	// 다음 깊이에서 작업이 끝나면 초기화 시켜준다
 			}
 		}
@@ -38,9 +38,9 @@ void dfs(int depth)
 int main()
 {
 	scanf("%d %d", &n, &m);
-	dfs(0);	// 깊이 0부터 시작
+	dfs(0, 1);	// 깊이 0부터 시작, 숫자는 1부터 시작
 
 	return 0;
 }
 
-// 참고 https://giantbaby1012.tistory.com/117
+// 참고 https://velog.io/@kimmainsain/C%EC%96%B8%EC%96%B4-%EB%B0%B1%EC%A4%80-15650-N%EA%B3%BC-M-2
