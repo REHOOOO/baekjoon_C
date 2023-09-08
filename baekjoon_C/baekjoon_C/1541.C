fdbcddeb37a220a8ff0ea68dd100567bc805610a
REHOOOO <ryueunhwan00@gmail.com>
2023-09-08 16:58:39 +0900

    1541
    + 가 나오면 앞에 -가 안나왔으면 더해줌  앞에 -가 한번이라도 나왔으면 빼 줌
    flag로 확인 할 것

---
+++ baekjoon_C/baekjoon_C/1541.C
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h> // atoi 함수 사용
+
+int main()
+{
+    int flag = 0;
+    int i,j;
+    char str[55];
+
+    scanf("%s", str);
+
+    for (i = 0; i < 55; i++)
+    {
+        if (str[i]=='+')
+        {
+            
+        }
+    }
+}
