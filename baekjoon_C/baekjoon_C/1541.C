590ea725f4f2237d2cccd053ee44dddfd2057e16
REHOOOO <104660000+REHOOOO@users.noreply.github.com>
2023-09-12 21:28:13 +0900

    1541

--- baekjoon_C/baekjoon_C/1541.C
+++ baekjoon_C/baekjoon_C/1541.C
@@ -1,21 +1,64 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h> // atoi 함수 사용
 
 int main()
 {
-    int flag = 0;
-    int i,j;
-    char str[55];
+	int flag = 0;
+	int minus = 0;
+	int i, j, k;
+	char str[55];
+	char temp[55];
+	int num = 0;
 
-    scanf("%s", str);
+	scanf("%s", str);
 
-    for (i = 0; i < 55; i++)
-    {
-        if (str[i]=='+')
-        {
-            
-        }
-    }
+	for (i = 0; i < 55; i++)
+	{
+		if (minus == 0) // - 가 아직 나오지 않았을 때 
+		{
+			if (str[i] == '+' || str[i] == '\0')
+			{
+				k = 0;
+				for (j = flag; j < i; j++)
+				{
+					temp[k] = str[j];
+					k++;
+				}
+				temp[k] = '\0';
+				flag = j + 1;
+				num = num + atoi(temp);
+			}
+			if (str[i] == '-')  // 가장 처음 -가 나오면
+			{
+				k = 0;
+				for (j = flag; j < i; j++)
+				{
+					temp[k] = str[j];
+					k++;
+				}
+				temp[k] = '\0';
+				flag = j + 1;
+				num = num + atoi(temp);
+				minus = 1;
+			}
+		}
+		else
+		{
+			if (str[i] == '+' || str[i] == '-' || str[i] == '\0')
+			{
+				k = 0;
+				for (j = flag; j < i; j++)
+				{
+					temp[k] = str[j];
+					k++;
+				}
+				temp[k] = '\0';
+				flag = j + 1;
+				num = num - atoi(temp);
+			}
+		}
+	}
+
+	printf("%d", num);
 }
