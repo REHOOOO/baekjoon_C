59b742b54f7762cea83dc92ba86e64b053cd1208
REHOOOO <104660000+REHOOOO@users.noreply.github.com>
2023-09-15 21:12:14 +0900

    1260
    DFS 함수 추가

--- baekjoon_C/baekjoon_C/1260.C
+++ baekjoon_C/baekjoon_C/1260.C
@@ -1,6 +1,41 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
+int n, m;
+int arr[8] = { 0, };
+int visit[8] = { 0, };
+int num[8] = { 0, };
+
+void dfs(int depth, int start)
+{
+	int i;
+	if (depth == m)	// 깊이가 m이면 현재까지 저장했던 수열을 출력
+	{
+		for (i = 0; i < m; i++)
+		{
+			printf("%d ", arr[i]);
+		}
+		printf("\n");
+	}
+	else
+	{
+		for (i = start; i < n; i++)
+		{
+			if (visit[i] == 1)	// 이미 사용한 숫자이면 넘어간다
+			{
+				continue;
+			}
+			else
+			{
+				arr[depth] = num[i];	// 현재 깊이의 숫자는 i로 저장
+				visit[i] = 1;	// 사용했다는 정보를 저장
+				dfs(depth + 1, i);	// 다음 깊이로 넘어간다 / 시작 숫자를 정해준다
+				visit[i] = 0;	// 다음 깊이에서 작업이 끝나면 초기화 시켜준다
+			}
+		}
+	}
+}
+
 int main()
 {
 	
