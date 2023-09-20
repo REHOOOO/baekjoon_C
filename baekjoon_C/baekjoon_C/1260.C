1c28dcda05de39e9f0b61617b28fb5031a858729
REHOOOO <104660000+REHOOOO@users.noreply.github.com>
2023-09-20 21:44:30 +0900

    1260

--- baekjoon_C/baekjoon_C/1260.C
+++ baekjoon_C/baekjoon_C/1260.C
@@ -4,13 +4,12 @@
 int n, m, v;
 int arr[1001] = { 0, };
 int visit[1001] = { 0, };
-int num[1001] = { 0, };
 int edge[1001][1001];
 int edgeflag[1001] = { 0, };
 
 void dfs(int depth, int start)
 {
-	int i;
+	int i,j;
 	if (depth == m)	// 깊이가 m이면 현재까지 저장했던 수열을 출력
 	{
 		for (i = 0; i < m; i++)
@@ -21,7 +20,7 @@ void dfs(int depth, int start)
 	}
 	else
 	{
-		for (i = start; i < n; i++)
+		for (i = start; i <= n; i++)
 		{
 			if (visit[i] == 1)	// 이미 사용한 숫자이면 넘어간다
 			{
@@ -29,8 +28,12 @@ void dfs(int depth, int start)
 			}
 			else
 			{
-				arr[depth] = num[i];	// 현재 깊이의 숫자는 i로 저장
+				arr[depth] = i;	// 현재 깊이의 숫자는 i로 저장
 				visit[i] = 1;	// 사용했다는 정보를 저장
+				for (j = 0; j < edgeflag[i]; j++)
+				{
+
+				}
 				dfs(depth + 1, i);	// 다음 깊이로 넘어간다 / 시작 숫자를 정해준다
 				visit[i] = 0;	// 다음 깊이에서 작업이 끝나면 초기화 시켜준다
 			}
@@ -52,6 +55,7 @@ int main()
 		edge[b][edgeflag[b]] = a;
 		edgeflag[a]++;
 		edgeflag[b]++;
-
 	}
+
+	dfs(0, v);
 }
