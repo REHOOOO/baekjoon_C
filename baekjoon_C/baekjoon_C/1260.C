b2ca3ff362da54e435f52a4daebd78e98f7cd1cf
REHOOOO <104660000+REHOOOO@users.noreply.github.com>
2023-09-22 18:02:56 +0900

    1260
    현재 방식에서는 처음 엣지에서만 bfs로 처리하고 다음 엣지로 이동하면 bfs와 dfs가 섞여서 작동

--- baekjoon_C/baekjoon_C/1260.C
+++ baekjoon_C/baekjoon_C/1260.C
@@ -10,39 +10,86 @@ int arrlen = 0;
 
 void dfs(int depth, int start)
 {
-	int i,j;
-	if (depth == n)	// 깊이가 m이면 현재까지 저장했던 수열을 출력
+	//////////// 초기화 //////////////
+	int i, j;
+	for (i = 0; i < 1001; i++)
 	{
-		for (i = 0; i < m; i++)
-		{
-			printf("%d ", arr[i]);
-		}
-		printf("\n");
+		arr[i] = 0;
+		visit[i] = 0;
 	}
-	else
+	arrlen = 0;
+	/////////////////////////////////
+
+	arr[depth] = start;
+	arrlen = depth;
+	visit[start] = 1;
+	for (i = 1; i <= n; i++)	// 오름차 순으로 숫자를 찾는다
 	{
-		arr[depth] = start;
-		arrlen = depth;
-		visit[start] = 1;
-		for (i = 1; i <= n; i++)	// 오름차 순으로 숫자를 찾는다
+		for (j = 0; j < edgeflag[start]; j++)
 		{
-			for (j = 0; j < edgeflag[start]; j++)	
+			if (i == edge[start][j])	// 오름차 순으로 숫자를 찾던 도중 현재 edge에 연결된 edge의 숫자가 나온다면 
 			{
-				if (i == edge[start][j])	// 오름차 순으로 숫자를 찾던 도중 현재 edge에 연결된 edge의 숫자가 나온다면 
+				if (visit[edge[start][j]] == 1)	// 이미 사용한 숫자이면 넘어간다
 				{
-					if (visit[edge[start][j]] == 1)	// 이미 사용한 숫자이면 넘어간다
-					{
-						continue;
-					}
-					else
-					{
-						dfs(depth + 1, edge[start][j]);	// 다음 깊이로 넘어간다 / 시작 숫자를 정해준다
-					}
+					continue;
+				}
+				else
+				{
+					dfs(depth + 1, edge[start][j]);	// 다음 깊이로 넘어간다 / 시작 숫자를 정해준다
 				}
 			}
 		}
-		
 	}
+
+
+}
+
+void bfs(int breadth, int start)
+{
+	//////////// 초기화 //////////////
+	int i, j;
+	for (i = 0; i < 1001; i++)
+	{
+		arr[i] = 0;
+		visit[i] = 0;
+	}
+	arrlen = 0;
+	/////////////////////////////////
+
+	if (breadth == 0)
+	{
+		arr[breadth] = start;
+		visit[start] = 1;
+	}
+	
+	for (i = 1; i <= n; i++) // arr에 현재 너비에 있는 숫자들 추가시켜주기
+	{
+		for (j = 0; j < edgeflag[start]; j++)	
+		{
+			if (i == edge[start][j])
+			{
+				if (visit[i] == 0)
+				{
+					breadth++;
+					arr[breadth] = i;
+					visit[i] = 1;
+				}
+			}
+		}
+	}
+
+	for (i = 1; i <= n; i++)
+	{
+		for (j = 0; j < edgeflag[start]; j++)	// 다음 edge들로 이동하기
+		{
+			if (i == edge[start][j])
+			{
+				
+			}
+		}
+	}
+
+
 }
 
 int main()
@@ -62,10 +109,11 @@ int main()
 	}
 
 	dfs(0, v);
-	for (i = 0; i <= arrlen; i++)	// n개 만큼 숫자를 출력해준다 (만약 n보다 작
+	for (i = 0; i <= arrlen; i++)	
 	{
 		printf("%d ", arr[i]);
 	}
 	printf("\n");
 
+	
 }
