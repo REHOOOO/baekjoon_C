c13e30ba551d1b9212c1b04079c4de82d70ba24b
REHOOOO <ryueunhwan00@gmail.com>
2023-09-25 14:41:36 +0900

    1012

---
+++ baekjoon_C/baekjoon_C/1012.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+int main()
+{
+    int t,m,n,k;
+    int i,j;
+    int xy[51][51]={0,};
+    int visit[51][51]={0,};
+    int x,y;
+    
+	scanf("%d",&t);
+
+    for(i=0;i<t;i++)
+    {
+        scanf("%d %d %d",&m,&n,&k);
+        for(j=0;j<k;k++)
+        {
+            scanf("%d %d",&x,&y);
+            xy[x][y]=1;
+        }
+    }
+}
