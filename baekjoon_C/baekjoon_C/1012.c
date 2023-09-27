54586c9a9012cf69b9597cfeee11c986db40d588
REHOOOO <104660000+REHOOOO@users.noreply.github.com>
2023-09-27 19:03:45 +0900

    1012
    끝자락에 있는 위치에서 visit을 확인할 때 다른 값을 읽게 됨

--- baekjoon_C/baekjoon_C/1012.c
+++ baekjoon_C/baekjoon_C/1012.c
@@ -5,20 +5,44 @@
 int main()
 {
 	int t, m, n, k;
-	int i, j;
+	int i, j, l;
 	int xy[51][51] = { 0, };
 	int visit[51][51] = { 0, };
 	int x, y;
+	int cnt;
 
 	scanf("%d", &t);
 
 	for (i = 0; i < t; i++)
 	{
 		scanf("%d %d %d", &m, &n, &k);
-		for (j = 0; j < k; k++)
+		for (j = 0; j < k; j++)
 		{
 			scanf("%d %d", &x, &y);
 			xy[x][y] = 1;
 		}
+
+		cnt = 0;
+
+		for (j = 0; j < m; j++)
+		{
+			for (l = 0; l < n; l++)
+			{
+				if (xy[j][l] == 1)
+				{
+					if (visit[j][l] == 0 && visit[j - 1][l] == 0 && visit[j][l - 1] == 0 && visit[j + 1][l] == 0 && visit[j][l + 1] == 0)
+					{
+						cnt++;
+						visit[j][l] = 1;
+					}
+					else
+					{
+						visit[j][l] = 1;
+					}
+				}
+			}
+		}
+
+		printf("%d\n", cnt);
 	}
 }
