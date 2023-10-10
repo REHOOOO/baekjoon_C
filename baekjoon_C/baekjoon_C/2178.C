8c44b3ead41272d8d4ff885b015c7c3ae6ccf749
REHOOOO <104660000+REHOOOO@users.noreply.github.com>
2023-10-10 20:36:30 +0900

    2178
    제곱 함수 추가
    입력 받은 값을 maze에 저장

--- baekjoon_C/baekjoon_C/2178.C
+++ baekjoon_C/baekjoon_C/2178.C
@@ -1,11 +1,23 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
+int power(int base, int exp)	// 제곱
+{
+	int i;
+	int num = 1;
+	for (i = 0; i < exp; i++)
+	{
+		num = num * base;
+	}
+
+	return num;
+}
+
 int main()
 {
-	int n,m;
-	int i;
-	int num;
+	int n, m;
+	int i, j;
+	int num, temp;
 	int maze[100][100] = { 0, };
 
 	scanf("%d %d", &n, &m);
@@ -14,6 +26,15 @@ int main()
 	{
 		scanf("%d", &num);
 
+		for (j = m; j > 0; j--)
+		{
+			temp = power(10, j - 1);
+
+			if ((num / temp) % 2 == 1)	// num을 temp로 나누었을 때 홀수인 경우
+			{
+				maze[m - j][i] = 1;
+			}
+		}
 	}
 
 	return 0;
