4031b18d303e13b519a8ddc1561f2276ccb22512
REHOOOO <ryueunhwan00@gmail.com>
2023-10-11 13:14:12 +0900

    2178
    문자열로 입력을 받도록 변경

--- baekjoon_C/baekjoon_C/2178.C
+++ baekjoon_C/baekjoon_C/2178.C
@@ -1,40 +1,28 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-int power(int base, int exp)	// 제곱
-{
-	int i;
-	int num = 1;
-	for (i = 0; i < exp; i++)
-	{
-		num = num * base;
-	}
-
-	return num;
-}
-
 int main()
 {
 	int n, m;
 	int i, j;
-	int num, temp;
+	int temp;
+	char num[100];
 	int maze[100][100] = { 0, };
 
 	scanf("%d %d", &n, &m);
 
 	for (i = 0; i < n; i++)
 	{
-		scanf("%d", &num);
-
-		for (j = m; j > 0; j--)
+		scanf("%s", &num);
+		for(j=0;j<m;j++)
 		{
-			temp = power(10, j - 1);
-
-			if ((num / temp) % 2 == 1)	// num을 temp로 나누었을 때 홀수인 경우
+			if(num[j]=='1')
 			{
-				maze[m - j][i] = 1;
+				maze[j][i]=1;
 			}
 		}
+
+		
 	}
 
 	return 0;
