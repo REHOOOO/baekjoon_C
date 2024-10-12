0871e6973af357380e874074598e8a08bbf0bc0c
REHOOOO <104660000+REHOOOO@users.noreply.github.com>
2024-10-13 05:41:03 +0900

    7576
    입력 부분

--- baekjoon_C/baekjoon_C/7576.C
+++ baekjoon_C/baekjoon_C/7576.C
@@ -1,3 +1,25 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
+
+int box[1000][1000] = { 0, };
+
+int main()
+{
+	int n, m;
+	int i, j;
+	
+
+	scanf("%d %d", &m, &n);
+
+	// 미로 입력
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < m; j++)
+		{
+			scanf("%d", &box[i][j]);
+		}
+	}
+
+	return 0;
+}
