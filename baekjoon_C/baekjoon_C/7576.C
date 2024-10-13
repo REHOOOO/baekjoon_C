f9d94ee88247708be3a4610511c19101255987ee
REHOOOO <104660000+REHOOOO@users.noreply.github.com>
2024-10-13 17:14:44 +0900

    7576

--- baekjoon_C/baekjoon_C/7576.C
+++ baekjoon_C/baekjoon_C/7576.C
@@ -1,18 +1,100 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-
+int n, m;
 int box[1000][1000] = { 0, };
+int queue[1000000][2] = { 0, };
+
+int BFS()
+{
+	int i, j;
+	int front = 0;
+	int rear = 0;
+	int max = 0;
+
+	// 초기 토마토 위치 찾기
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < m; j++)
+		{
+			if (box[i][j]==1)
+			{
+				queue[rear][0] = j;
+				queue[rear][1] = i;
+				rear++;
+			}
+		}
+	}
+
+	while (front < rear)
+	{
+		int x = queue[front][0];
+		int y = queue[front][1];
+		front++;
+
+		if ((y + 1 < n) && (box[y + 1][x] == 0))	// 아래가 0일 경우
+		{
+			box[y + 1][x] = box[y][x] + 1;
+			queue[rear][0] = x;
+			queue[rear][1] = y + 1;
+			rear++;
+		}
+
+		if ((x + 1 < m) && (box[y][x + 1] == 0))	// 오른쪽이 0일 경우
+		{
+			box[y][x + 1] = box[y][x] + 1;
+			queue[rear][0] = x + 1;
+			queue[rear][1] = y;
+			rear++;
+		}
+
+		if ((y - 1 >= 0) && (box[y - 1][x] == 0))	// 위가 0일 경우
+		{
+			box[y - 1][x] = box[y][x] + 1;
+			queue[rear][0] = x;
+			queue[rear][1] = y - 1;
+			rear++;
+		}
+
+		if ((x - 1 >= 0) && (box[y][x - 1] == 0))	// 왼쪽이 0일 경우
+		{
+			box[y][x - 1] = box[y][x] + 1;
+			queue[rear][0] = x - 1;
+			queue[rear][1] = y;
+			rear++;
+		}
+	}
+
+	// 토마토의 상태 확인
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < m; j++)
+		{
+			if (box[i][j] == 0)	// 익지 못한 토마토가 있을 시 
+			{
+				printf("%d", -1);
+				return 0;
+			}
+			else if (max < box[i][j])
+			{
+				max = box[i][j];
+			}
+		}
+	}
+
+	printf("%d", max-1);
+
+	return 0;
+}
+
 
 int main()
 {
-	int n, m;
 	int i, j;
 	
-
 	scanf("%d %d", &m, &n);
 
-	// 미로 입력
+	// 입력
 	for (i = 0; i < n; i++)
 	{
 		for (j = 0; j < m; j++)
@@ -20,6 +102,8 @@ int main()
 			scanf("%d", &box[i][j]);
 		}
 	}
+
+	BFS();
 
 	return 0;
 }
