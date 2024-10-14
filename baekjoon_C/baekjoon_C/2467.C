0e8db093537af685cd8b2e1ca4aaae0e143c5a13
REHOOOO <104660000+REHOOOO@users.noreply.github.com>
2024-10-14 15:23:07 +0900

    2467

--- baekjoon_C/baekjoon_C/2467.C
+++ baekjoon_C/baekjoon_C/2467.C
@@ -8,9 +8,10 @@ int solution[100000] = { 0, };
 int main()
 {
 	int n;
-	int i, j;
-	int min = 1000000000;
+	int i;
+	double min = 3000000000;
 	int result[2] = { 0, };
+	int left, right;
 
 	scanf("%d", &n);
 
@@ -19,24 +20,37 @@ int main()
 		scanf("%d", &solution[i]);
 	}
 
-	for (i = 0; i < n; i++)
+	left = 0;
+	right = n - 1;
+
+	while (solution[left] < solution[right])
 	{
-		for (j = i + 1; j < n; j++)
+		double calc = solution[left] + solution[right];	// 계산값
+
+		if (calc == 0)	// 계산값이 0일 경우
 		{
-			if (abs(solution[i] + solution[j]) == 0)
-			{
-				result[0] = solution[i];
-				result[1] = solution[j];
-				break;
-			}
-			else if (min > abs(solution[i] + solution[j]))
-			{
-				min = abs(solution[i] + solution[j]);
-				result[0] = solution[i];
-				result[1] = solution[j];
-			}
+			result[0] = solution[left];
+			result[1] = solution[right];
+			break;
+		}
+		else if (min > abs(calc))	// 계산값이 min보다 작을 경우
+		{
+			min = abs(calc);
+			result[0] = solution[left];
+			result[1] = solution[right];
+		}
+
+		if (calc > 0)	// 계산값이 양수일 경우
+		{
+			right--;
+		}
+		else if (calc < 0)	//계산값이 음수일 경우
+		{
+			left++;
 		}
 	}
 
 	printf("%d %d", result[0], result[1]);
+
+	return 0;
 }
