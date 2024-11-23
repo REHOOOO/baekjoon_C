bd36682a4830c985bfc619795feee1c99ab27071
REHOOOO <104660000+REHOOOO@users.noreply.github.com>
2024-11-24 02:19:40 +0900

    1806
    런타임 에러  (OutOfBounds)

---
+++ baekjoon_C/baekjoon_C/1806.C
@@ -0,0 +1,47 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int N, S;
+	int i, j;
+	int num[10000];
+	int sum, result;
+
+	scanf("%d %d", &N, &S);
+
+	for (i = 0; i < N; i++)
+	{
+		scanf("%d", &num[i]);
+	}
+
+	result = 100000001;
+	for (i = 0; i < N; i++)
+	{
+		sum = num[i];
+		for (j = i + 1; j < N; j++)
+		{
+			sum = sum + num[j];	// sum에 num[j]를 더해준다
+			if (sum > S)	// 만약 sum이 S보다 커진다면
+			{
+				if (result > (j - i + 1))	// result보다 선택된 수의 개수가 작다면 result에 sum을 저장
+				{
+					result = (j - i + 1);
+				}
+				break;
+			}
+		}
+
+		if (result == 1)	// 만약 sum이 1이라면 (최소값)
+		{
+			break;
+		}
+	}
+
+	if (result == 100000001)
+	{
+		result = 0;
+	}
+
+	printf("%d", result);
+}
