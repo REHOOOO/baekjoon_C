d8f960c2521a5dbf6bd5d22b2ae5ff003f993794
REHOOOO <104660000+REHOOOO@users.noreply.github.com>
2024-12-20 03:02:54 +0900

    1005
    DFS 적용 중

--- baekjoon_C/baekjoon_C/1005.C
+++ baekjoon_C/baekjoon_C/1005.C
@@ -1,13 +1,30 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
+
+int graph[1000][1000] = { 0, };
+int visit[1000] = { 0, };
+
+int dfs(int node)
+{
+	visit[node] = 1;
+
+	for (int i = node-1; i >= 0 ; i--)
+	{
+		if (graph[node][i] == 1 && !visit[i])
+		{
+			dfs(i);
+		}
+	}
+}
+
 int main()
 {
 	int T;
 	int N, K;
 	int D[1000];
-	int X[100000];
-	int Y[100000];
+	int X, Y;
+	
 	int W;
 
 	scanf("%d", &T);
@@ -22,7 +39,9 @@ int main()
 		}
 		for (int j = 0; j < K; j++)
 		{
-			scanf("%d %d", &X[j], &Y[j]);
+			scanf("%d %d", &X, &Y);
+			graph[X-1][Y-1] = 1;
+			graph[Y-1][X-1] = 1;
 		}
 		
 		scanf("%d", &W);
