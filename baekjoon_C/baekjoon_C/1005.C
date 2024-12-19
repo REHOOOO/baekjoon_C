32783f792ad101c3d01a63e879bc8544c8ac2739
REHOOOO <104660000+REHOOOO@users.noreply.github.com>
2024-12-19 21:11:57 +0900

    1005
    입력 부분

---
+++ baekjoon_C/baekjoon_C/1005.C
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int T;
+	int N, K;
+	int D[1000];
+	int X[100000];
+	int Y[100000];
+	int W;
+
+	scanf("%d", &T);
+	for (int i = 0; i < T; i++)
+	{
+		////// ют╥б ///////
+		scanf("%d %d", &N, &K);
+
+		for (int j = 0; j < N; j++)
+		{
+			scanf("%d", &D[j]);
+		}
+		for (int j = 0; j < K; j++)
+		{
+			scanf("%d %d", &X[j], &Y[j]);
+		}
+		
+		scanf("%d", &W);
+
+
+	}
+	
+}
