6e28578ae6b9672c071ca722ae77181201902b4b
REHOOOO <104660000+REHOOOO@users.noreply.github.com>
2024-12-23 22:46:13 +0900

    2252
    입력 생성

---
+++ baekjoon_C/baekjoon_C/2252.C
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int N; // 학생 수 
+	int M; // 키를 비교한 횟수 
+	int front, rear;
+
+	for (int i = 0; i < N; i++)
+	{
+		scanf("%d %d", &front, &rear);
+
+	}
+}
