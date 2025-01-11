9c881c2c5c20585e0b33dd4689d0687f7252820e
REHOOOO <104660000+REHOOOO@users.noreply.github.com>
2025-01-12 04:17:47 +0900

    9466
    시작 노드가 포함되지 않은 순환을 만나게 되면 무한 루프에 빠짐
    visit을 추가하여 중간에 순환을 만나더라도 순환임을 확인하여 해결

--- baekjoon_C/baekjoon_C/9466.C
+++ baekjoon_C/baekjoon_C/9466.C
@@ -4,41 +4,30 @@
 
 int pick[100000];
 int check[100000];
+int visit[100000];
 int cnt;
 
-void dfs(int start, int node)
+void dfs(int node)
 {
-	if (check[node] == 1 || check[node] == 2)	// 이미 확인된 그룹일 경우 
+	visit[node] = 1;	// 방문 표시
+	int next = pick[node];	// 다음 가르키는 노드를 저장
+
+	if (visit[next] == 0)	// 다음 노드가 방문되지 않은 노드라면
 	{
+		dfs(next);	// 다음 노드로 이동
+	}
+	else if (check[next] == 0)	// 방문은 되었지만 확인이 안되어있다면 순환이 발생
+	{
+		while (check[node] == 0)	// 순환 내에 있는 노드들을 모두 확인 표시 해준다
+		{
+			check[node] = 1;	// 확인 표시
+			node = pick[node];	// 다음 노드
+			cnt--;	// 카운트 감소 
+		}
 		return;
 	}
 
-	if (pick[node] == start)	// 시작 노드와 다음 노드가 같다면 
-	{
-		check[node] = 1;	// 확인 표시
-		cnt--;	// 카운트를 줄여준다 
-
-		return;
-	}
-
-	if (node == pick[node])	// 현재 노드가 자신을 가르킬 경우 
-	{
-		check[node] = 2;	// 확인 표시
-		cnt--;	// 카운트를 줄여준다 
-		return;
-	}
-
-	dfs(start, pick[node]);	// 다음 연결 노드로 이동 
-
-	if (check[pick[node]] == 1)	// 다음 노드에서 확인표시가 생겼으면 
-	{
-		check[node] = 1;	// 확인 표시
-		cnt--;
-		return;
-	}
-
-
-
+	check[node] = 1;	// 다음 노드가 방문되었고 확인도 되어있다면 순환에 들어가지 못함 (확인 표시)
 	return;
 }
 
@@ -54,6 +43,7 @@ int main()
 		cnt = 0;
 		memset(check, 0, sizeof(check));
 		memset(pick, 0, sizeof(pick));
+		memset(visit, 0, sizeof(visit));
 
 		scanf("%d", &n);
 		cnt = n;
@@ -65,7 +55,7 @@ int main()
 
 		for (int j = 0; j < n; j++)
 		{
-			dfs(j, j);
+			dfs(j);
 
 		}
 
