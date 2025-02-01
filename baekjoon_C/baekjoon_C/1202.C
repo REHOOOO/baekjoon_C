288ca4be45a0ec9710d0e7a1be8e29d4434f2180
REHOOOO <104660000+REHOOOO@users.noreply.github.com>
2025-02-01 22:02:10 +0900

    1202
    우선순위 큐를 사용해서 해결
    또한 result의 크기가 int의 크기를 넘어가 long long int를 사용

--- baekjoon_C/baekjoon_C/1202.C
+++ baekjoon_C/baekjoon_C/1202.C
@@ -8,13 +8,62 @@ typedef struct
 	int V;	// 보석 가격
 } data;
 
-int compare_bag(const void* a, const void* b) { //오름차순으로 정렬 
+int compare_bag(const void* a, const void* b) //오름차순으로 정렬 
+{ 
 	return (*(int*)a - *(int*)b);
 }
 
-int compare_jewel(const void* a, const void* b) { // 보석 가격 기준 내림차순으로 정렬
-	return (((data*)b)->V - ((data*)a)->V);
+int compare_jewel(const void* a, const void* b) // 보석 무게 기준 오름차순으로 정렬
+{
+	return (((data*)a)->M - ((data*)b)->M);
 }
+
+///// 우선순위 큐 /////
+int heap[300000];
+int heap_size = 0;
+
+void push(int value) 
+{
+	heap[++heap_size] = value;
+	int current = heap_size;
+
+	while (current > 1 && heap[current] > heap[current / 2]) 
+	{
+		int temp = heap[current];
+		heap[current] = heap[current / 2];
+		heap[current / 2] = temp;
+		current /= 2;
+	}
+}
+
+int pop() 
+{
+	if (heap_size == 0) return 0;
+
+	int result = heap[1];
+	heap[1] = heap[heap_size--];
+	int current = 1;
+
+	while (current * 2 <= heap_size) 
+	{
+		int child = current * 2;
+		if (child < heap_size && heap[child] < heap[child + 1]) 
+		{
+			child++;
+		}
+		if (heap[current] >= heap[child])
+		{
+			break;
+		}
+		int temp = heap[current];
+		heap[current] = heap[child];
+		heap[child] = temp;
+		current = child;
+	}
+
+	return result;
+}
+/////////////////////////////////
 
 data jewel[300000];
 int C[300000];	// 가방 최대 무게
@@ -36,30 +85,22 @@ int main()
 	}
 
 	qsort(C, K, sizeof(int), compare_bag);	// 가방을 오름차순으로 정렬
-	qsort(jewel, N, sizeof(data), compare_jewel);	// 보석을 가격 내림차순으로 정렬
+	qsort(jewel, N, sizeof(data), compare_jewel);	// 보석을 무게 오름차순으로 정렬
 
-	int result = 0;	// 결과 계산
-	int available = K;
-	for (int i = 0; i < N; i++)	// 보석 번호
+	long long int result = 0;	// 결과 계산
+	int j = 0;		// 보석 번호
+
+	for (int i = 0; i < K; i++)	// i는 가방 번호
 	{
-		for (int j = 0; j < K; j++)	// 가방 번호
+		while (j < N && jewel[j].M <= C[i])	// 아직 큐에 안들어간 보석이 있고 현재 보석이 가방무게보다 가볍거나 같다면
 		{
-			if (C[j] > 0)	// 가방이 사용 가능하다면
-			{
-				if (C[j] > jewel[i].M)
-				{
-					result += jewel[i].V;	// 보석 가격 계산
-					C[j] = -1;	// 가방 사용 표시
-					available--;	// 남은 가방 수 
-					break;
-				}
-			}
+			push(jewel[j].V);	// 보석의 가격을 우선순위 큐에 넣어준다 (비싼 보석이 우선순위를 가진다)
+			j++;	// 보석 번호를 변경
 		}
-		if (available < 1)	// 남은 가방이 없으면 루프 종료
-		{
-			break;
-		}
+
+		result += pop();	// 가장 가치가 높은 보석의 가격을 더해준다 (큐가 비어있을 경우 0)
 	}
 
-	printf("%d", result);
+
+	printf("%lld", result);
 }
